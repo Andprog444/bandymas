8372c54fc00ce8896e1646f919d53c141f8594f7
Andprog444 <a.bystrimovicius@gmail.com>
2021-12-12 11:29:05 +0200

    delete files

--- script.js
+++
@@ -1,6 +0,0 @@
-function doSomething(){
-    let elem = document.getElementById("my-div");
-    elem.style.border = "1px solid brown";
-    elem.style.padding = "10px";
-    elem.style.margin = "10px";
-}
